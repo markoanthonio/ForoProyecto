002df89e83bc1548f33dd7bfae92aad9bb9b232c
Bvsty <alvarezbastian7@gmail.com>
2023-05-01 19:56:02 -0400

    val

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 $(document).ready(function(){
-  $("#registrar").validate({
-    rules:{
-      name:{
-        required: true,
-        minlength: 3,
-      },
-      lastname:{
-        required: true,
-        minlength: 3
-      },
-      email:{
-        required: true,
-        email: true
-      }
+  $("#registrar").submit(function(e){
+    e.preventDefault();
+    var nombre=$("#name").val();
+    var correo=$("#email").val();
+    var clave=$("#password").val();
+
+    var msjMostrar="";
+    let enviar=false;
+
+    if(nombre.trim().length < 4 || nombre.trim().length > 10){
+      msjMostrar+="El nombre debe tener entre 4 y 10 caracteres";
+      enviar=true;
+    }
+    var letra=nombre.trim().charAt(0);
+    if(!esMayuscula(letra)){
+      msjMostrar+="<br>La primera letra debe ser mayúscula";
+      enviar=true;
+    }
+
+    if(correo.trim().length<4 || correo.trim().length>10){
+      msjMostrar+="Formato invalido"
+      enviar=true;
+    }
+
+    if(clave.trim().length<4 || clave.trim().length>10){
+      msjMostrar+="La contraseña debe tener entre 4 y 10 caracteres";
+      enviar=true;
+    }
+
+    if(enviar){
+      $("#warnings").html(msjMostrar);
+
+    }
+    else{
+      $("#warnings").html("Enviado");
     }
   });
 });
